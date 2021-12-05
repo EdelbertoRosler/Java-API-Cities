b8126a9a73da9a17fe84414b5032629f186f8ac3
EdelbertoRosler <edelbertorosler@gmail.com>
2021-12-04 23:33:49 -0300

    commit inicial. Criação do projeto via Spring Initializr

---
+++ src/main/java/com/github/EdelbertoRosler/javaapicities/JavaApiCitiesApplication.java
@@ -0,0 +1,13 @@
+package com.github.EdelbertoRosler.javaapicities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiCitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiCitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/EdelbertoRosler/javaapicities/JavaApiCitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.EdelbertoRosler.javaapicities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiCitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
